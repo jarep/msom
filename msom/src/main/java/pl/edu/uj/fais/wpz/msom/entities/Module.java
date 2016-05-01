f546d400137722a15981fafa357671a613fb0755
Paweł Dyląg <p.dyloo@gmail.com>
2016-05-01 16:31:38 +0200

    Added module entity

---
+++ msom/src/main/java/pl/edu/uj/fais/wpz/msom/entities/Module.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.uj.fais.wpz.msom.entities;
+
+import javax.persistence.Entity;
+import pl.edu.uj.fais.wpz.msom.entities.abstracts.AbstractEntity;
+
+/**
+ *
+ * @author paweldylag
+ */
+@Entity
+public class Module extends AbstractEntity {
+
+    private Integer cores;
+    private Integer efficiency;
+    
+    public Module() {}
+
+    public Module(Integer cores, Integer efficiency) {
+        this.cores = cores;
+        this.efficiency = efficiency;
+    }
+
+    public Integer getCores() {
+        return cores;
+    }
+
+    public void setCores(Integer cores) {
+        this.cores = cores;
+    }
+
+    public Integer getEfficiency() {
+        return efficiency;
+    }
+
+    public void setEfficiency(Integer efficiency) {
+        this.efficiency = efficiency;
+    }
+    
+    
+    
+    
+    
+    
+   
+}
