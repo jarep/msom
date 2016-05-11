9ae15b3e93780abba82e11b320047d7a62b67f6d
Paweł Dyląg <p.dyloo@gmail.com>
2016-05-11 21:38:34 +0200

    Added Distribution entity

---
+++ msom/src/main/java/pl/edu/uj/fais/wpz/msom/entities/Distribution.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.uj.fais.wpz.msom.entities;
+
+import javax.persistence.Entity;
+import pl.edu.uj.fais.wpz.msom.entities.abstracts.AbstractEntity;
+
+/**
+ * Basic Module (Single server) entity structure
+ * @author paweldylag
+ */
+@Entity
+public class Distribution extends AbstractEntity {
+    
+    /** Distribution name */
+    private String name;
+
+    public Distribution(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
