d560d445ebc3a90c67c5de17e059b58dc4a7423a
Olaf <ziomek88>
2016-05-30 21:49:36 +0200

    improved simulation mock

--- msom-angular2/src/app/simulation.service.ts
+++ msom-angular2/src/app/simulation.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http'
 import {Observable} from "rxjs/Observable";
-import {TaskDispatcher, Task, ProcessingSystem} from './shared/contracts'
+import {TaskDispatcher, Task, ProcessingSystem, ProcessingUnit} from './shared/contracts'
 @Injectable()
 export class SimulationService {
 
@@ -16,11 +16,26 @@ export class SimulationService {
           //        return res.json()
           //    }
           //)
+          var processingSystem = getProcessingSystem();
           return Observable.interval(500).map(() => 
-              getProcessingSystem()
+               inc(processingSystem)
           )
       }
 }
+var inc : (processingSystem : ProcessingSystem) => ProcessingSystem = (processingSystem) => {
+     processingSystem.taskDispatchers.forEach(x => x.processingUnits.forEach(p => p.tasks.forEach(t => { 
+        var inProgress = Math.random() > 0.5 ? true : false;
+        t.id =  t.id,
+        t.inProgress = inProgress,
+        t.type= t.type,
+        t.processed = inProgress ? +(t.processed + Math.random()*2).toFixed(2) : t.processed,
+        t.totalWaitingTime = +(t.totalWaitingTime+ Math.random()).toFixed(2),
+        t.totalProcessingTime = +(t.totalProcessingTime+ Math.random()).toFixed(2),
+        t.processingCount = +Math.round(Math.random() * 10)
+    })))
+    return processingSystem;
+}
+
 var getProcessingSystem : () => ProcessingSystem = () => {
     return {
         id : "12",
@@ -51,9 +66,9 @@ var getTask : () => Task = () => {
     return {
         id: "10",
         type: Math.random() > 0.66 ? "A" : Math.random() > 0.5 ? "B" : "C" ,
-        processed : +(Math.random() * 100).toFixed(2),
-        totalWaitingTime : +(Math.random() * 100).toFixed(2),
-        totalProcessingTime :+(Math.random() * 100).toFixed(2),
+        processed : +(Math.random().toFixed(2)),
+        totalWaitingTime : +(Math.random()).toFixed(2),
+        totalProcessingTime :+(Math.random()).toFixed(2),
         processingCount : +Math.round(Math.random() * 10),
         inProgress: Math.random() > 0.5 ? true : false
     }
