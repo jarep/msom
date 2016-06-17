1fd655422d8049868a5b8e17ca2ba4dbc80476a2
rafter31 <tyborrafal@gmail.com>
2016-06-18 01:18:35 +0200

    add hide/show buttons

--- msom/src/main/webapp/resources/js/processingsystems.js
+++ msom/src/main/webapp/resources/js/processingsystems.js
@@ -1,3 +1,4 @@
+var isHidden = [];
     $(document).ready(function(){
         var addr = document.location.href;
         var x = $("#isLocked").val();
@@ -10,8 +11,26 @@
                    cache : false,
                    success : function(data) {
                        $(".simulation-container").html(data);
+                       checkHiddenElements();
                    }
                 });
             }, 1000);
         }
     });
+
+    function toggleDetails(self)
+    {   
+        $(self.parentNode).find('.tasks-list').toggle(); 
+        if(isHidden.includes(self.parentElement.children[2].className))
+        isHidden.splice( $.inArray(self.parentElement.children[2].className, isHidden), 1 );
+        else
+          isHidden.push(self.parentElement.children[2].className);
+    }
+
+     function checkHiddenElements(){
+         for (var i = 0;i<isHidden.length;i++)
+         {
+             $("[class*='"+isHidden[i]+"']").hide();
+         }
+     }
+     
